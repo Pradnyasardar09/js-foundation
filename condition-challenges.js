22991fbd40e74457253bc759b323b86bc93c4602
Pradnyaaaa <pradnyasardar2005@gmail.com>
2025-03-16 21:28:31 +0530

    Add files via upload

---
+++ condition-challenges.js
@@ -0,0 +1,23 @@
+//check if a number is greater than  another number:
+
+
+let num1 = 5;
+let num2 = 8;
+//console.log["im regular  upper  code" ];
+
+if (num1 > num2) {
+//console.log("num1 is greater than num2");
+}
+    //console.log("nope , num1 is NOT grater");
+
+    //console.log("im regular  bottom  code" );
+
+    //check if String is equal to another  string;
+
+    let username =  "Eren"
+    let anotherusername = "Eren"
+
+    if (username == anotherusername){
+        console.log(["pick another username"]);
+        
+    }
