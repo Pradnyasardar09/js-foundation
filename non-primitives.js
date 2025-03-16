c74231771231393556f28d499f5ad59ea7c2e6a9
Pradnyaaaa <pradnyasardar2005@gmail.com>
2025-03-16 21:22:01 +0530

    Add files via upload

---
+++ non-primitives.js
@@ -0,0 +1,31 @@
+let username = {
+    firstname : "rahul", 
+    isgoingout: true
+};
+
+
+
+
+username.firstname = "mrs. R";
+username.lastname = "biter";
+
+//console.log(username.firstname);
+//console.log(username.lastname);
+
+//console.log(typeof username);
+
+
+let today = (Date);
+//console.log(today. getDate());
+
+//array
+let anotheruser =["Eren", "yeager" ,true]
+//console.log(anotheruser[0]);
+
+
+let isValue = "2";
+console.log( typeof number(isValue));
+
+
+
+
