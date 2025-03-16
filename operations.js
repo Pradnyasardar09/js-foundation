b8c1eb08aa76d1417fafdbab97bea7e9dd564525
Pradnyaaaa <pradnyasardar2005@gmail.com>
2025-03-16 21:23:34 +0530

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,35 @@
+let score = 102
+let bonus = 25
+
+let totalscore = score + bonus
+
+
+
+let additon = 4 + 5
+let subtraction = 9 - 3
+let multiplication = 3 * 9
+let divi = 3 / 2
+let reminder = 9 % 2
+let expo = 2 ** 3
+
+let myscore = 100
+myscore++
+++score
+
+let credits = 55
+credits--;
+
+
+//comparion operation
+
+let num1 = 3
+let num2 = 3
+let num3 = 6
+
+console.log(num1 == num2);
+console.log(num1 != num3);
+
+
+
+
+
