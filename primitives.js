192d62182898f643ce67b1f042cb5a77e75839b3
Pradnyaaaa <pradnyasardar2005@gmail.com>
2025-03-16 21:24:21 +0530

    Add files via upload

---
+++ primitives.js
@@ -0,0 +1,49 @@
+ //number
+
+ let balance = 120;
+
+ let anotherBalance = new Number(120)
+
+// console.log(balance);
+ //console.log(anotherBalance. valueOf());
+ 
+ 
+ 
+ //console.log(typeof balance);
+// console.log(typeof anotherBalance);
+
+
+//boolean
+let isActive = true
+let isrealActive = new Boolean(true) //not recomended
+
+
+// null and undefind
+
+let firstname = null;
+let lastname = undefined;
+//console.log(firstname);
+//console.log(lastname);
+
+//string
+
+let myString = "hello"
+let myStringOne = 'hola'
+let username = 'daddy'
+
+let oldGreetMessage = myString +   "  "  + "daddy";
+
+//console.log(oldGreetMessage);
+
+let greetMessage = `hello ${username} !`;
+//console.log(greetMessage);
+let demoOne = `value is ${2 *2}`;
+//console.log(demoOne);
+
+let sm1 = Symbol()
+let sm2 = Symbol()
+
+console.log(sm1 == sm2);
+
+
+ 
