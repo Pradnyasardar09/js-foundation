f2719ced0d253de375b58030843b30fe8fa9a0fa
Pradnyaaaa <pradnyasardar2005@gmail.com>
2025-03-16 21:20:10 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,36 @@
+console.log("hello");
+
+
+
+//cosole.log("daddy");
+//*
+//String
+//Boolean
+//Number
+//Bigint
+
+
+//undefined
+//null
+
+//object
+
+//symbol
+//*
+
+
+//var score = 102
+
+let score =102;
+let name ="code.com";
+let islogging = false;
+
+//object
+
+letfashiontype = ["long dress","mini dress","suite"]
+let user ={firstname:"pradnya" , lastname:"sardar"}
+
+
+console.log("letscore");
+
+
